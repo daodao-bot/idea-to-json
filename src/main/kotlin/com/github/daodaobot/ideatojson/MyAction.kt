eaf194e19512ab990afef96145bf9026a88d5027
GitHub Action <action@github.com>
2023-08-05 16:01:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/daodaobot/ideatojson/MyAction.kt
+++ src/main/kotlin/com/github/daodaobot/ideatojson/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.daodaobot.ideatojson
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/daodaobot/ideatojson/MyBundle.kt
+++ src/main/kotlin/com/github/daodaobot/ideatojson/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.daodaobot.ideatojson
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
